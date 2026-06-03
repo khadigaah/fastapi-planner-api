04a5485eef14dc1f1ad4a801455e3d5d4c16486f
khadiga <khadiga.dev@gmail.com>
2026-06-03 09:46:42 +0300

    in connection.py create a new database.

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,10 @@
-from typing import Optional
+from typing import Any, List, Optional
 
 from beanie import init_beanie
 from models.events import Event
 from models.users import User
-from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseSettings
+from motor.motor_asyncio import AsyncIOMotorClient, PydanticObjectId
+from pydantic import BaseModel, BaseSettings
 from sqlalchemy import Session, SQLModel, create_engine
 
 
@@ -31,3 +31,46 @@ def conn():
 def get_session():
     with Session(engine_url) as session:
         yield session
+
+
+class Database(BaseModel):
+    def __init__(self, model):
+        self.model = model
+
+    #create a new document in the database
+    async def save(self, document) -> None:
+        await document.create()
+        return
+
+    #Read a document from the database by its id
+    async def get(self, id : PydanticObjectId) -> Any:
+        doc = await self.model.get(id)
+        if doc:
+            return doc
+        return False
+
+    async def get_all(self) -> List[Any]:
+        docs = await self.model.find_all().to_list()
+        return docs
+
+    #Update a document in the database by its id
+    async def update(self, id: PydanticObjectId, body: BaseModel) -> Any:
+        doc_id = id
+        des_body = body.dict()
+        des_body = {k: v for k, v in des_body.items() if v is not None}
+        update_query = {"$set": {field: value for field, value in des_body.items()}}
+
+        doc = await self.get(doc_id)
+        if not doc:
+            return False
+        await doc.update(update_query)
+        return doc
+
+#Delete a document from the database by its id
+    async def delete(self, id: PydanticObjectId) -> Any:
+        doc = await self.get(id)
+        if not doc:
+            return False
+        await doc.delete()
+        return True
+
